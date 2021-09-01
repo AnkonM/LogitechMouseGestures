1b9db7bf42312582ddcb9844c2ba812073359c0b
BigDaddyMoisture <73531637+BigDaddyMoisture@users.noreply.github.com>
2021-09-01 08:12:19 +0530

    added script w/ custom gestures

---
+++ G304Gestures.lua
@@ -0,0 +1,110 @@
+--[[
+Author:		BigDaddyMoisture aka LordToothpaste
+Version:	0.8 
+Date: 		30.08.2021
+
+Special thanks to wookiefriseur for the help :)
+
+Keys:
+    Win(Super): "lgui"
+    L-Alt: "lalt"
+    L-Control: "lctrl" ?????
+    Arrow-keys: "up", "left", "down", "right"
+
+This script attempts to implement gesture button functionality similar to other logitech mice that have customizable gesture buttons.
+The gestures use before and after comparisons of mouse pointer positions.
+
+To execute a gesture move the mouse pointer while holding the special button:
+* up to open task view 		(Winkey+Tab)
+* down to minimise all windows	(Winkey+D)
+* left to move to desktop at relative RIGHT	(Winkey+Ctrl+Right)
+* right to move desktop at relative RIGHT	(Winkey+Ctrl+Left)
+
+Mouse buttons:
+1=M1,2=M2,3=MMB and so on. For G-buttons, their respective numbers 1:1. 
+--]]
+
+button_number = 6; -- Mouse button number
+threshold_x = 5000; -- x,y thresholds
+threshold_y = 8000; 
+show_console_output = false;
+
+-- enable / disable gestures
+taskv_on = true;		-- maximise window
+min_on = true;		-- minimise window
+left_on = true;		-- move to left
+right_on = true;	-- move to right
+
+-- inits
+pos_x_start, pos_y_start = 0, 0;
+pos_x_end, pos_y_end = 0, 0;
+pos_string = "";
+diff_string = "";
+
+-- Event listener
+function OnEvent(event, arg, family)
+	if event == "MOUSE_BUTTON_PRESSED" and arg == button_number then
+		Sleep(30);
+		pos_x_start, pos_y_start = GetMousePosition();
+		Sleep(20);
+        -- Console output text
+		pos_string = "x: " .. pos_x_start .. " y: " .. pos_y_start;
+	end
+
+	if event == "MOUSE_BUTTON_RELEASED" and arg == button_number then
+		Sleep(30);
+		pos_x_end, pos_y_end = GetMousePosition();
+		Sleep(20);
+		diff_x = pos_x_end - pos_x_start;
+		diff_y = pos_y_start - pos_y_end;
+        -- Console output text
+        pos_string = "x: " .. pos_x_end .. " y: " .. pos_y_end;
+		diff_string = "x: " .. diff_x .. " y: " .. diff_y;
+
+		-- Gestures
+		if taskv_on and diff_y > threshold_y then gtaskv_oniew() end
+		if min_on and diff_y < -threshold_y then gMinimiseAll() end
+		if right_on and diff_x > threshold_x then gMoveDesktopLeft() end		
+		if left_on and diff_x < -threshold_x then gMoveDesktopRight() end
+	end
+--OutputLogMessage("event = %s, arg = %s, family=%s\nPosition = %s, Movement= %s \n", event, arg, family,pos_string,diff_string); --debug
+end
+
+-- Gesture actions
+function gtaskv_oniew()
+	pressTwoKeys("lgui","tab",20);
+	if show_console_output then
+     	OutputLogMessage("[TASKVIEW]\t[Position = %s, Movement= %s ]\n",pos_string,diff_string);
+    end
+end
+
+function gMinimiseAll()
+	pressTwoKeys("lgui","d",20);
+        if show_console_output then
+        OutputLogMessage("[MINIMISE]\t[Position = %s, Movement= %s ]\n",pos_string,diff_string);
+    end
+end
+
+function gMoveDesktopLeft()
+	PlayMacro("MoveDesktopLeft");
+    	if show_console_output then
+     	OutputLogMessage("[MOVE LEFT]\t[Position = %s, Movement= %s ]\n",pos_string,diff_string);
+	end
+end
+
+function gMoveDesktopRight()
+	PlayMacro("MoveDesktopRight");
+     if show_console_output then
+     	OutputLogMessage("[MOVE RIGHT]\t[Position = %s, Movement= %s ]\n",pos_string,diff_string);
+	end
+end
+
+-- Helper functions
+function pressTwoKeys(key1,key2,ms)
+	PressKey(key1);
+	Sleep(ms);
+	PressKey(key2);
+	Sleep(ms);
+	ReleaseKey(key2);
+	ReleaseKey(key1);
+end
