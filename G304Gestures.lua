10b2bbd458c7ed61457a9ef66c2a40301658128c
BigDaddyMoisture <73531637+BigDaddyMoisture@users.noreply.github.com>
2021-09-01 09:17:41 +0530

    Add custom 2-way DPI cycle combos
    mb6 + mb5 -> increase dpi
    mb6 + mb4 -> decrease dpi

--- G304Gestures.lua
+++ G304Gestures.lua
@@ -44,6 +44,7 @@ diff_string = "";
 -- Event listener
 function OnEvent(event, arg, family)
 	if event == "MOUSE_BUTTON_PRESSED" and arg == button_number then
+		mb6_pressed = true
 		Sleep(30);
 		pos_x_start, pos_y_start = GetMousePosition();
 		Sleep(20);
@@ -51,7 +52,24 @@ function OnEvent(event, arg, family)
 		pos_string = "x: " .. pos_x_start .. " y: " .. pos_y_start;
 	end
 
+	-- 1/09
+	if event == "MOUSE_BUTTON_PRESSED" and arg == 4 then
+		mb4_pressed = true
+	end
+	if event == "MOUSE_BUTTON_RELEASED" and arg == 4 then
+		mb4_pressed = false
+	end
+
+	if event == "MOUSE_BUTTON_PRESSED" and arg == 5 then
+		mb5_pressed = true
+	end
+	if event == "MOUSE_BUTTON_RELEASED" and arg == 5 then
+		mb5_pressed = false
+	end
+	-- end 1/09
+
 	if event == "MOUSE_BUTTON_RELEASED" and arg == button_number then
+		mb6_pressed = false
 		Sleep(30);
 		pos_x_end, pos_y_end = GetMousePosition();
 		Sleep(20);
@@ -62,16 +80,26 @@ function OnEvent(event, arg, family)
 		diff_string = "x: " .. diff_x .. " y: " .. diff_y;
 
 		-- Gestures
-		if taskv_on and diff_y > threshold_y then gtaskv_oniew() end
+		if taskv_on and diff_y > threshold_y then gtaskView() end
 		if min_on and diff_y < -threshold_y then gMinimiseAll() end
 		if right_on and diff_x > threshold_x then gMoveDesktopLeft() end		
 		if left_on and diff_x < -threshold_x then gMoveDesktopRight() end
 	end
+
+	-- 1/09
+	if event == "MOUSE_BUTTON_RELEASED" and arg == 4 then
+		if mb6_pressed == true then dpiDOWN() end
+	end
+
+	if event == "MOUSE_BUTTON_RELEASED" and arg == 5 then
+		if mb6_pressed == true then dpiUP() end
+	end
+	-- end 1/09
 --OutputLogMessage("event = %s, arg = %s, family=%s\nPosition = %s, Movement= %s \n", event, arg, family,pos_string,diff_string); --debug
 end
 
 -- Gesture actions
-function gtaskv_oniew()
+function gtaskView()
 	pressTwoKeys("lgui","tab",20);
 	if show_console_output then
      	OutputLogMessage("[TASKVIEW]\t[Position = %s, Movement= %s ]\n",pos_string,diff_string);
@@ -97,6 +125,14 @@ function gMoveDesktopRight()
      if show_console_output then
      	OutputLogMessage("[MOVE RIGHT]\t[Position = %s, Movement= %s ]\n",pos_string,diff_string);
 	end
+end
+
+function dpiUP()
+	PlayMacro("dpiUp");
+end
+
+function dpiDOWN()
+	PlayMacro("dpiDown");
 end
 
 -- Helper functions
